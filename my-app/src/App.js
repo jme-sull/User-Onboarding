1b8579e03f25fc93d7e98b9cae7653aa58e24fa1
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-05-21 06:24:22 -0400

    form works, post request works, users saved in state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import './components/Form';
+import UserForm from './components/Form';
+import { useState } from 'react';
+import axios from 'axios'
+
+const intaialFormValues = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  password: '',
+  terms_of_service: false 
+}
+
 
 function App() {
+
+  const [ formValues, setFormValues ] = useState(intaialFormValues)
+  const [ users, addUsers ] = useState([{}])
+
+
+  const onInputChange = evt => {
+    const name = evt.target.name
+    const value = evt.target.value
+
+    setFormValues({
+      ...formValues, [name]:value
+    })
+  }
+
+  const onCheckboxChange = evt => {
+
+    const name = evt.target.name
+    const checked = evt.target.checked
+
+    setFormValues({
+      ...formValues, [name]: checked
+    })
+  }
+
+  const onSubmit = evt => {
+
+    evt.preventDefault()
+
+    const newUser = {
+      first_name: formValues.first_name.trim(),
+      last_name: formValues.last_name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms_of_service: formValues.terms_of_service
+    }
+
+    postNewUser(newUser)
+
+
+
+  }
+
+const postNewUser = newUser => {
+  axios.post('https://reqres.in/api/users', newUser)
+  .then(res => {
+    addUsers([res.data, ...users])
+    console.log(users)
+  })
+  .catch(err => {
+    debugger
+  })
+  .finally(() => {
+    setFormValues(intaialFormValues)
+  })
+
+}
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>The Users</h1>
+        
+
+        <div>
+          <UserForm values={formValues} onInputChange={onInputChange} 
+          onCheckboxChange={onCheckboxChange} onSubmit={onSubmit}/>
+        </div>
       </header>
     </div>
   );
